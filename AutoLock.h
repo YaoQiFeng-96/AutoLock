43dfed5633715f1dc2bab473b749b1c4c77a707e
YaoQiFeng-96 <996601826@qq.com>
2020-06-01 11:54:16 +0800

    note off

--- AutoLock.h
+++ AutoLock.h
@@ -10,12 +10,12 @@ public:
 	AutoLock(pthread_mutex_t *pMutex):m_pMutex(pMutex)
 	{
 		pthread_mutex_lock(m_pMutex);
-		std::cout<<"mutex locked."<<std::endl;
+		// std::cout<<"mutex locked."<<std::endl;
 	}
 	~AutoLock()
 	{
 		pthread_mutex_unlock(m_pMutex);
-		std::cout<<"mutex unlocked."<<std::endl;
+		// std::cout<<"mutex unlocked."<<std::endl;
 	}
 private:
 	pthread_mutex_t *m_pMutex;
